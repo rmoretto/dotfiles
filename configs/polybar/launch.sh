31cf16acd872c717145df0e243e575e67260f2ea
rmoretto <rodrigo.ce.moretto@gmail.com>
2020-08-01 16:18:45 -0300

    added network interface passing via environent variable to the polybar secondary bar

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -16,12 +16,13 @@ SECONDARY_MONITOR=$(xrandr | grep " connected|^primary" | cut -f1 -d' ')
 
 if [ ${IS_LAPTOP} -eq 0 ]; then
   # We are in the Desktop land, use the default bars
+
   MONITOR=${PRIMARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload main &
-  [ -n "${SECONDARY_MONITOR}" ] && MONITOR=${SECONDARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload secondary &
+  [ -n "${SECONDARY_MONITOR}" ] && MONITOR=${SECONDARY_MONITOR} NET_IF=enp3s0 polybar -c ~/.config/polybar/config.ini --reload secondary &
 else
   # We are in the Laptop land, use the default bars
   MONITOR=${PRIMARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload main_notebook &
-  [ -n "${SECONDARY_MONITOR}" ] && MONITOR=${SECONDARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload secondary_notebook &
+  [ -n "${SECONDARY_MONITOR}" ] && MONITOR=${SECONDARY_MONITOR} NET_IF=wlo1 polybar -c ~/.config/polybar/config.ini --reload secondary_notebook &
 fi
 
 echo "Bars launched..."
