e7075699d23dc9d8e0add7ab37683de2d9769590
rmoretto <rodrigo.ce.moretto@gmail.com>
2020-08-01 16:38:21 -0300

    siplified arrow colors

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -8,7 +8,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Verify if we are using a laptop or desktop for choosing the bars
 DMI_DESKTOP_ID=3
-[ "$(cat /sys/class/dmi/id/chassis_type)" -ne ${DMI_DESKTOP_ID} ] && echo IS_LAPTOP=1 || IS_LAPTOP=0
+[ "$(cat /sys/class/dmi/id/chassis_type)" -ne ${DMI_DESKTOP_ID} ] && IS_LAPTOP=1 || IS_LAPTOP=0
+
+echo "Is laptop: ${IS_LAPTOP}"
 
 # Get the primary and secondary monitors
 PRIMARY_MONITOR=$(xrandr | grep " connected primary" | cut -f1 -d' ')
@@ -16,8 +18,8 @@ SECONDARY_MONITOR=$(xrandr | grep " connected|^primary" | cut -f1 -d' ')
 
 if [ ${IS_LAPTOP} -eq 0 ]; then
   # We are in the Desktop land, use the default bars
-  MONITOR=${PRIMARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload main &
-  [ -n "${SECONDARY_MONITOR}" ] && MONITOR=${SECONDARY_MONITOR} NET_IF=enp3s0 polybar -c ~/.config/polybar/config.ini --reload secondary &
+  MONITOR=${PRIMARY_MONITOR} NET_IF=enp3s0 polybar -c ~/.config/polybar/config.ini --reload main &
+   [ -n "${SECONDARY_MONITOR}" ] && MONITOR=${SECONDARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload secondary &
 else
   # We are in the Laptop land, use the default bars
   MONITOR=${PRIMARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload main_notebook &
