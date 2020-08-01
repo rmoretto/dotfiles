b35694915558ce5808e1fac58814fedc1b1bc06a
rmoretto <rodrigo.ce.moretto@gmail.com>
2020-08-01 16:07:08 -0300

    happy pycharm

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Verify if we are using a laptop or desktop for choosing the bars
 DMI_DESKTOP_ID=3
-[ $(cat /sys/class/dmi/id/chassis_type) -ne ${DMI_DESKTOP_ID} ] && echo IS_LAPTOP=1 || IS_LAPTOP=0
+[ "$(cat /sys/class/dmi/id/chassis_type)" -ne ${DMI_DESKTOP_ID} ] && echo IS_LAPTOP=1 || IS_LAPTOP=0
 
 # Get the primary and secondary monitors
 PRIMARY_MONITOR=$(xrandr | grep " connected primary" | cut -f1 -d' ')
@@ -17,11 +17,11 @@ SECONDARY_MONITOR=$(xrandr | grep " connected|^primary" | cut -f1 -d' ')
 if [ ${IS_LAPTOP} -eq 0 ]; then
   # We are in the Desktop land, use the default bars
   MONITOR=${PRIMARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload main &
-  [ ! -z ${SECONDARY_MONITOR} ] && MONITOR=${SECONDARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload secondary &
+  [ -n "${SECONDARY_MONITOR}" ] && MONITOR=${SECONDARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload secondary &
 else
   # We are in the Laptop land, use the default bars
   MONITOR=${PRIMARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload main_notebook &
-  [ ! -z ${SECONDARY_MONITOR} ] && MONITOR=${SECONDARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload secondary_notebook &
+  [ -n "${SECONDARY_MONITOR}" ] && MONITOR=${SECONDARY_MONITOR} polybar -c ~/.config/polybar/config.ini --reload secondary_notebook &
 fi
 
 echo "Bars launched..."
