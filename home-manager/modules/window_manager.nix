409d5d66e109393b447eae1096bbc123c0b11c99
rmoretto <rodrigo.ce.moretto@gmail.com>
2024-02-05 21:14:34 -0300

    fix gtk conf

--- home-manager/modules/window_manager.nix
+++ home-manager/modules/window_manager.nix
@@ -114,6 +114,14 @@
     + "/extras/.Xresources"
   );
 
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 12;
+  };
+
   gtk = {
     enable = true;
     theme = {
